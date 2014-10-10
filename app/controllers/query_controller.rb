8fa0ca1ca2ec929d033e2b898b00c00420da69bb
jksharer <zk.amazing@gmail.com>
2014-10-10 12:07:18 +0800

    add export query results to excel function.

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -12,27 +12,27 @@ class QueryController < ApplicationController
   def query_via_class
   	start_date = params[:start_date]
   	end_date = Date.parse(params[:end_date]) + 1
-  	puts params[:confirm_state]
 		case params[:iclass]
 		when "all_classes"
 			case params[:behavior_type]
 			when "all_types"
 				case params[:address]
 				when "所有地点"
-					@behaviors = Behavior.where("time >= ?", start_date).where("time <= ?", end_date).where(confirm_state: params[:confirm_state])
+					@behaviors = Behavior.where("time >= ?", start_date).where("time <= ?", end_date).where(confirm_state: params[:confirm_state]).
+						order('student_id asc')
 					# @behaviors = Behavior.where(time: start_date..end_date).where(confirm_state: params[:confirm_state])					
 				else
 					@behaviors = Behavior.where("time >= ?", start_date).where("time <= ?", end_date).where(confirm_state: params[:confirm_state]).
-						where(address: params[:address])
+						where(address: params[:address]).order('student_id asc')
 				end
 			else
 				case params[:address]
 				when "所有地点"
 					@behaviors = Behavior.where("time >= ?", start_date).where("time <= ?", end_date).where(confirm_state: params[:confirm_state]).
-						where(behavior_type_id: params[:behavior_type])
+						where(behavior_type_id: params[:behavior_type]).order('student_id asc')
 				else
 					@behaviors = Behavior.where("time >= ?", start_date).where("time <= ?", end_date).where(confirm_state: params[:confirm_state]).
-						where(behavior_type_id: params[:behavior_type]).where(address: params[:address])
+						where(behavior_type_id: params[:behavior_type]).where(address: params[:address]).order('student_id asc')
 				end
 			end
 		else
@@ -44,25 +44,25 @@ class QueryController < ApplicationController
 				case params[:address]
 				when "所有地点"
 					@behaviors = Behavior.where("time >= ?", start_date).where("time <= ?", end_date).where(confirm_state: params[:confirm_state]).
-						where(student_id: student_ids)	
+						where(student_id: student_ids).order('student_id asc')	
 				else
 					@behaviors = Behavior.where("time >= ?", start_date).where("time <= ?", end_date).where(confirm_state: params[:confirm_state]).
-						where(student_id: student_ids).where(address: params[:address])
+						where(student_id: student_ids).where(address: params[:address]).order('student_id asc')
 				end
 			else
 				case params[:address]
 				when "所有地点"
 					@behaviors = Behavior.where("time >= ?", start_date).where("time <= ?", end_date).where(confirm_state: params[:confirm_state]).
-						where(student_id: student_ids).where(behavior_type_id: params[:behavior_type])
+						where(student_id: student_ids).where(behavior_type_id: params[:behavior_type]).order('student_id asc')
 				else
 					@behaviors = Behavior.where("time >= ?", start_date).where("time <= ?", end_date).where(confirm_state: params[:confirm_state]).
-						where(student_id: student_ids).where(behavior_type_id: params[:behavior_type]).where(address: params[:address])
+						where(student_id: student_ids).where(behavior_type_id: params[:behavior_type]).where(address: params[:address]).order('student_id asc')
 				end
 			end
 		end
 		respond_to do |format|
       format.html {
-  			redirect_to query_via_class_path(format: 'xls')
+  			redirect_to query_via_class_path(params.merge(:format => :xls))
   		}
       format.js { render 'query.js.erb' }
       format.csv { send_data @behaviors.to_csv }
